c58da0c26312937716050c43097dfccaf8c6b7f1
Keidson Roby <keidsondesigner@gmail.com>
2021-06-18 22:28:19 -0400

    Array Revisao

--- aula90arrayRevisao.js
+++ aula90arrayRevisao.js
@@ -1,26 +1,22 @@
 const pessoa = {
-    nome: 'Luiz',
-    sobrenome: 'Otávio',
-    idade: 35,
-}
+	nome: "Luiz",
+	sobrenome: "Otávio",
+	idade: 35,
+};
 console.log(pessoa.nome);
 
-
-
 const pessoa2 = new Object();
-pessoa2.name = 'Keidson';
-pessoa2.lastName = 'Roby';
+pessoa2.name = "Keidson";
+pessoa2.lastName = "Roby";
 pessoa2.age = 30;
 
 pessoa2.falaNome = function () {
-    console.log(`${this.name}`);
+	console.log(`${this.name}`);
 };
 pessoa2.falaNome();
 
-
 pessoa2.getAnoNascimento = function () {
-    const anoAtual = new Date();
-    return anoAtual.getFullYear() - this.age;
+	const anoAtual = new Date();
+	return anoAtual.getFullYear() - this.age;
 };
 console.log(pessoa2.getAnoNascimento());
-
