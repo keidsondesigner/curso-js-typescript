8266887563a74807f1e87aadfa33c6fcf3a35aa5
Keidson Roby <keidsondesigner@gmail.com>
2021-06-18 21:49:21 -0400

    funcao new Date

--- aula90arrayRevisao.js
+++ aula90arrayRevisao.js
@@ -5,9 +5,22 @@ const pessoa = {
 }
 console.log(pessoa.idade);
 
-const pessoa2 = new Object();
-pessoa2.nome = 'Keidson';
-pessoa2.sobrenome = 'Roby';
 
-delete pessoa2.nome;
-console.log(pessoa2);
+
+const pessoa2 = new Object();
+pessoa2.name = 'Keidson';
+pessoa2.lastName = 'Roby';
+pessoa2.age = 30;
+
+pessoa2.falaNome = function () {
+    console.log(`${this.name}`);
+};
+pessoa2.falaNome();
+
+
+pessoa2.getAnoNascimento = function () {
+    const anoAtual = new Date();
+    return anoAtual.getFullYear() - this.age;
+};
+console.log(pessoa2.getAnoNascimento());
+
