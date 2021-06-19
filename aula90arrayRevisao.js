0adc2ec382c39687a6cb3b5bd9b50ef05e68974d
Keidson Roby <keidsondesigner@gmail.com>
2021-06-18 21:13:37 -0400

    aula 90

--- aula90arrayRevisao.js
+++ aula90arrayRevisao.js
@@ -3,5 +3,11 @@ const pessoa = {
     sobrenome: 'Otávio',
     idade: 35,
 }
-
 console.log(pessoa.idade);
+
+const pessoa2 = new Object();
+pessoa2.nome = 'Keidson';
+pessoa2.sobrenome = 'Roby';
+
+delete pessoa2.nome;
+console.log(pessoa2);
