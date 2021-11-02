0b5168114e45c62144f2ddae05ecadb9b5a38547
keidsondesigner <keidsondesigner@gmail.com>
2021-11-01 21:19:01 -0400

    atuazliando obj

--- revisao-1/objetos.js
+++ revisao-1/objetos.js
@@ -1,13 +1,15 @@
-const pessoa = {
-  nome: 'Keidson',
-  sobrenome: 'Roby'
-}
-
-console.log(pessoa['nome'])
-console.log(pessoa.sobrenome)
+const pessoa = new Object()
+pessoa.nome = 'Keidson'
+pessoa.sobrenome = 'Roby'
+pessoa.idade = 30
 
 pessoa.falarNome = function () {
   console.log(`${this.nome} está falando seu nome`)
 }
-
 pessoa.falarNome()
+
+pessoa.getDataNascimento = function () {
+  const dataAtual = new Date()
+  return dataAtual.getFullYear() - this.idade
+}
+console.log(pessoa.getDataNascimento())
